b1035ad1ef7406ce37ebaa29ab5de64e136ba091
Naoko Shoto <me@naokoshoto.my.id>
2026-09-01 08:21:30 +0200

    beryl: Welcome PixelOS
    ```
    Pixelated kisses got me goin' insane
    Replicate this moment from a million miles away
    Waiting for the signal, baby, never make a sound
    If you never hear from me, all the satellites are down
    Yeah, they're all fuckin' down
    ```
    
    Change-Id: I3d278f55599b1576fe8d2d4b383c79132fa6504c

--- custom_beryl.mk
+++ custom_beryl.mk
@@ -9,13 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/halcyon/config/common.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
 
 # Inherit from beryl device
 $(call inherit-product, device/xiaomi/beryl/device.mk)
 
 PRODUCT_DEVICE := beryl
-PRODUCT_NAME := halcyon_beryl
+PRODUCT_NAME := custom_beryl
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 14 5G
 PRODUCT_MANUFACTURER := Xiaomi
