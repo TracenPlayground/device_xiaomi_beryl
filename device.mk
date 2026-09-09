5fbf7f6cc5e83a30e898b2b26c5a9747e62f31f5
Naoko Shoto <me@naokoshoto.my.id>
2026-09-09 14:06:06 +0200

    beryl: Drop obsolete mtkpower 1.2 HIDL service stub
    Drop obsolete vendor.mediatek.hardware.mtkpower@1.2-service.stub which
    fails registration on Android 16 target-level without hwservicemanager.
    
    Change-Id: Ia43b3eccfc4ab7a0c41e9999ac001a30b4b58ccd
    Signed-off-by: Naoko Shoto <me@naokoshoto.my.id>

--- device.mk
+++ device.mk
@@ -238,7 +238,6 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set,power_libperfmgr,mode_extension_lib, //$(LOCAL_PATH):libperfmgr-ext-xiaomi)
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     libmtkperf_client_vendor \
     libmtkperf_client \
     libpowerhalwrap_vendor
